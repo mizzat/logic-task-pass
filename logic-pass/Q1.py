861aff6d414a91e838cd35bcb9f7ecad18531354
mizzat <mohammedizzat96@gmail.com>
2022-08-11 02:03:20 +0300

    Update Q1.py

--- logic-pass/Q1.py
+++ logic-pass/Q1.py
@@ -3,8 +3,8 @@ str=('Life is not good')
 x=1
 while (x<2):
     print("This is our string: "+str)
-    wanted_character = input("What do you want to remove: ")
-    if wanted_character in str:
+    rm_character = input("What do you want to remove: ")
+    if rm_character in str:
         str=str.replace(wanted_character,'')
         print(str)
     else:
